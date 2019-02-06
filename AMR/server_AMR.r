9d41b1fa8e67e9210592e4487047037a1746dd5f
nandadorea <dorea.meyer@gmail.com>
2019-02-06 15:38:21 +0100

    works on my computer.... :/

--- AMR/server_AMR.r
+++ AMR/server_AMR.r
@@ -1,11 +1,11 @@
 # load data ----
-library(stringr)
+#library(stringr)
 load(paste0(sourcefiles,"/SVA3D/AMR/AMR_total.RData"))
 source(paste0(sourcefiles,"/SVA3D/SVA3D_functions.r"))
 source(paste0(sourcefiles,"/SVA3D/SVA3D_definitions.r"))
 
-columns.res <- eliminate.swedish.the.wrong.way(colnames(res))
-colnames(res) <- columns.res
+#columns.res <- eliminate.swedish.the.wrong.way(colnames(res))
+#colnames(res) <- columns.res
 
 # res1 ----
 res1 <- reactive({
