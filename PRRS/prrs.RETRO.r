48a048b5cb2cc136bfab71683ae4df83642fb502
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2019-08-28 14:36:31 +0200

    Function to read and combine retro data for all years. Filtering to retrieve PRRS-relevant pigs, construction of timeseries and plotting

--- PRRS/prrs.RETRO.r
+++ PRRS/prrs.RETRO.r
@@ -29,26 +29,98 @@ library(stringr)
 #    prrs.sva.retro3,
 #    prrs.sva.retro4)
 
-html_pattern <- "<[^>]*>(.+)<[^>]*>"
+read.retro.data <- function(path, start_year, end_year) {
+  ret <- data.frame()
+  
+  html_pattern <- "<[^>]*>(.+)<[^>]*>"
+  
+  paths <-
+    str_c(path, "prrs.retro.", as.character(seq.int(start_year, end_year)), ".txt")
+  
+  for (p in paths) {
+    data <-
+      read.csv2(
+        p,
+        sep = "\t",
+        header = TRUE,
+        na.strings = "",
+        stringsAsFactors = FALSE,
+        encoding = "UTF-8",
+        quote = ""
+      ) %>%
+      filter(Djurslag == "Svin" | Djurslag == "Gris") %>%
+      mutate(
+        Uppdrag = ifelse(
+          str_detect(Uppdrag, html_pattern),
+          str_replace(Uppdrag, html_pattern, "\\1"),
+          Uppdrag
+        ),
+        Ankomstdatum = as.Date(Ankomstdatum, "%m/%d/%y")
+      )
+    
+    ret <- rbind(ret, data)
+  }
+  return(ret)
+}
 
-prrs.sva.retro <-
-  read.csv2(
-    "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2018.txt",
-    sep = "\t",
-    header = TRUE,
-    na.strings = "",
-    stringsAsFactors = FALSE,
-    encoding = "UTF-8",
-    quote = ""
-  ) %>%
-  mutate(Uppdrag = ifelse(str_detect(Uppdrag, html_pattern), str_replace(Uppdrag, html_pattern, "\\1"), Uppdrag))
+# prrs.sva.2016 <-
+#   read.csv2(
+#     "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2016.txt",
+#     sep = "\t",
+#     header = TRUE,
+#     na.strings = "",
+#     stringsAsFactors = FALSE,
+#     encoding = "UTF-8",
+#     quote = ""
+#   ) %>%
+#   mutate(Uppdrag = ifelse(
+#     str_detect(Uppdrag, html_pattern),
+#     str_replace(Uppdrag, html_pattern, "\\1"),
+#     Uppdrag
+#   ))
+# 
+# prrs.sva.2017 <-
+#   read.csv2(
+#     "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2017.txt",
+#     sep = "\t",
+#     header = TRUE,
+#     na.strings = "",
+#     stringsAsFactors = FALSE,
+#     encoding = "UTF-8",
+#     quote = ""
+#   ) %>%
+#   mutate(Uppdrag = ifelse(
+#     str_detect(Uppdrag, html_pattern),
+#     str_replace(Uppdrag, html_pattern, "\\1"),
+#     Uppdrag
+#   ))
+# 
+# prrs.sva.2018 <-
+#   read.csv2(
+#     "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2018.txt",
+#     sep = "\t",
+#     header = TRUE,
+#     na.strings = "",
+#     stringsAsFactors = FALSE,
+#     encoding = "UTF-8",
+#     quote = ""
+#   ) %>%
+#   mutate(Uppdrag = ifelse(
+#     str_detect(Uppdrag, html_pattern),
+#     str_replace(Uppdrag, html_pattern, "\\1"),
+#     Uppdrag
+#   ))
+
+prrs.sva.retro <- read.retro.data("I:/ESS/SVA3D/PRRS/retro data/", 2009, 2018)
 
 # import list of possible agens and their relevancy to PRRS
 agens.prrs.list <-
-    read.csv2(file = "PRRS/prrs.agens.csv",
-            encoding = "UTF-8",
-            colClasses = "character",
-            na.strings = "")
+  read.csv2(
+    file = "I:/ESS/SVA3D/PRRS/prrs.agens.csv",
+    encoding = "UTF-8",
+    colClasses = "character",
+    na.strings = ""
+  )
 
 # extract only the agens which have been marked relevant to PRRS, and discard irrelevant columns
 agens.relevant <- agens.prrs.list %>%
@@ -58,7 +130,7 @@ agens.relevant <- agens.prrs.list %>%
 
 undersokning.prrs.list <-
   read.csv2(
-    file = "PRRS/prrs.undersokning.csv",
+    file = "I:/ESS/SVA3D/PRRS/prrs.undersokning.csv",
     encoding = "UTF-8",
     colClasses = "character",
     na.strings = ""
@@ -72,7 +144,10 @@ undersokning.relevant <- undersokning.prrs.list %>%
 # Find rows in dataset that have relevant agens and undersökning and attach value to them
 prrs.sva.retro <- prrs.sva.retro %>%
   left_join(agens.relevant, by = c("Agens" = "agens")) %>%
-  left_join(undersokning.relevant, by = c("Undersökning.Kod" = "undersokning", "Analys.Kod" = "undersokning"))
+  left_join(
+    undersokning.relevant,
+    by = c("Undersökning.Kod" = "undersokning", "Analys.Kod" = "undersokning")
+  )
 
 # set all other rows' values to 0
 prrs.sva.retro <- prrs.sva.retro %>%
@@ -80,13 +155,16 @@ prrs.sva.retro <- prrs.sva.retro %>%
     PRRSagens = ifelse(is.na(final.x), 0, final.x),
     PRRSundersokning = ifelse(is.na(final.y), 0, final.y)
   ) %>%
-  select(-final.x,-final.y)
+  select(-final.x, -final.y)
 
 prrs.sva.interesting <- prrs.sva.retro %>%
   filter(PRRSagens > 0 | PRRSundersokning > 0)
 
 prrs.passive.orsak <- prrs.sva.interesting %>%
-  filter(Provtagningsorsak.1 == "Epizootisjukdom/ -misstanke" | Provtagningsorsak.1 == "Sjukdom/Skada/Dödsfall")
+  filter(
+    Provtagningsorsak.1 == "Epizootisjukdom/ -misstanke" |
+      Provtagningsorsak.1 == "Sjukdom/Skada/Dödsfall"
+  )
 
 prrs.no.orsak <- prrs.sva.interesting %>%
   filter(is.na(Provtagningsorsak.1))
@@ -94,15 +172,43 @@ prrs.no.orsak <- prrs.sva.interesting %>%
 prrs.no.orsak.numberless <- prrs.no.orsak %>%
   filter(is.na(Överordnade.uppdrag))
 
-prrs.sva.interesting <- rbind(prrs.passive.orsak, prrs.no.orsak.numberless) %>%
-  mutate(Ankomstdatum = as.Date(Ankomstdatum, "%m/%d/%y"))
+prrs.sva.interesting <-
+  rbind(prrs.passive.orsak, prrs.no.orsak.numberless)
 
-interesting.timeseries <- prrs.sva.interesting %>%
-  group_by(Ankomstdatum) %>%
+interesting.events <- prrs.sva.interesting %>%
+  group_by(Uppdrag) %>%
+  summarise(date = Ankomstdatum[1])
+
+date.range <-
+  as.data.frame(seq.Date(as.Date("2012-01-01"), as.Date("2018-12-31"), by = "day"))
+colnames(date.range) <- "date"
+
+interesting.timeseries <- interesting.events %>%
+  group_by(date) %>%
   count() %>%
-  complete(Ankomstdatum = seq.Date(as.Date("2018-01-01"), as.Date("2018-12-31"), by = "day")) %>%
-  mutate(n = ifelse(is.na(n), 0, n))
+  right_join(date.range, by = "date") %>%
+  mutate(
+    n = ifelse(is.na(n), 0, n),
+    doy = as.numeric(strftime(date, format = "%j")),
+    m = month(date),
+    w = week(date),
+    y = as.factor(year(date))
+  )
 
 interesting.timeseries %>%
-  ggplot(aes(x = Ankomstdatum, y = n)) +
+  ggplot(aes(doy, n, color = y)) +
+  geom_line()
+
+interesting.timeseries %>%
+  group_by(w, y) %>%
+  summarise(n = sum(n)) %>%
+  ggplot(aes(x = w, y = n, color = y)) +
+  geom_line()
+
+interesting.timeseries %>%
+  group_by(y, w) %>%
+  summarise(n = sum(n)) %>%
+  group_by(w) %>%
+  summarise(n = mean(n)) %>%
+  ggplot(aes(w, n)) +
   geom_line()
