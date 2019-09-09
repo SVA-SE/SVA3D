31d23904e6dabf7c9581c4bca29466ecbc317758
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2019-09-09 16:42:06 +0200

    data cleaning, fitlering on PRRS_relevant parameters and timeseries construction for SVA and SJV data respectively

--- PRRS/prrs.RETRO.r
+++ PRRS/prrs.RETRO.r
@@ -1,214 +1,224 @@
 Sys.setlocale("LC_ALL", "swedish")
 
-library(tidyverse)
 library(lubridate)
 library(stringr)
+library(gtools)
+library(data.table)
+library(ggplot2)
 
-# read the 4 data files into data frames
-
-# prrs.sva.retro1 <-
-#   read.csv2(
-#     "I:/ESS/SVA3D/PRRS/retro data/syndromic.gris.1.txt",
-#     sep = "\t",
-#     header = TRUE,
-#     na.strings = "",
-#     stringsAsFactors = FALSE,
-#     encoding = "UTF-8",
-#     quote = ""
-#   )
-
-
-# # combine into one data frame and remove the original data frames
-# prrs.sva.retro <-
-#   rbind(prrs.sva.retro1,
-#             prrs.sva.retro2,
-#             prrs.sva.retro3,
-#             prrs.sva.retro4)
-# rm(prrs.sva.retro1,
-#    prrs.sva.retro2,
-#    prrs.sva.retro3,
-#    prrs.sva.retro4)
-
-read.retro.data <- function(path, start_year, end_year) {
-  ret <- data.frame()
+make_timeseries <- function(data, daterange) {
+  ret <-
+    merge(data[, .N, by = date][order(date)],
+          daterange,
+          by = "date",
+          all = TRUE)[, c("doy", "week", "month", "year") := list(as.numeric(strftime(date, format = "%j")),
+                                                                  week(date),
+                                                                  month(date),
+                                                                  as.factor(year(date)))]
+  ret <- ret[, yearweek :=  str_c(as.character(year), as.character(week), sep = "-")]
+  ret[is.na(N)]$N <- 0
   
-  html_pattern <- "<[^>]*>(.+)<[^>]*>"
-  
-  paths <-
-    str_c(path, "prrs.retro.", as.character(seq.int(start_year, end_year)), ".txt")
-  
-  for (p in paths) {
-    data <-
-      read.csv2(
-        p,
-        sep = "\t",
-        header = TRUE,
-        na.strings = "",
-        stringsAsFactors = FALSE,
-        encoding = "UTF-8",
-        quote = ""
-      ) %>%
-      filter(Djurslag == "Svin" | Djurslag == "Gris") %>%
-      mutate(
-        Uppdrag = ifelse(
-          str_detect(Uppdrag, html_pattern),
-          str_replace(Uppdrag, html_pattern, "\\1"),
-          Uppdrag
-        ),
-        Ankomstdatum = as.Date(Ankomstdatum, "%m/%d/%y")
-      )
-    
-    ret <- rbind(ret, data)
-  }
+  factor()
+  ret <- ret[]
   return(ret)
 }
 
-# prrs.sva.2016 <-
-#   read.csv2(
-#     "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2016.txt",
-#     sep = "\t",
-#     header = TRUE,
-#     na.strings = "",
-#     stringsAsFactors = FALSE,
-#     encoding = "UTF-8",
-#     quote = ""
-#   ) %>%
-#   mutate(Uppdrag = ifelse(
-#     str_detect(Uppdrag, html_pattern),
-#     str_replace(Uppdrag, html_pattern, "\\1"),
-#     Uppdrag
-#   ))
-# 
-# prrs.sva.2017 <-
-#   read.csv2(
-#     "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2017.txt",
-#     sep = "\t",
-#     header = TRUE,
-#     na.strings = "",
-#     stringsAsFactors = FALSE,
-#     encoding = "UTF-8",
-#     quote = ""
-#   ) %>%
-#   mutate(Uppdrag = ifelse(
-#     str_detect(Uppdrag, html_pattern),
-#     str_replace(Uppdrag, html_pattern, "\\1"),
-#     Uppdrag
-#   ))
-# 
-# prrs.sva.2018 <-
-#   read.csv2(
-#     "I:/ESS/SVA3D/PRRS/retro data/prrs.retro.2018.txt",
-#     sep = "\t",
-#     header = TRUE,
-#     na.strings = "",
-#     stringsAsFactors = FALSE,
-#     encoding = "UTF-8",
-#     quote = ""
-#   ) %>%
-#   mutate(Uppdrag = ifelse(
-#     str_detect(Uppdrag, html_pattern),
-#     str_replace(Uppdrag, html_pattern, "\\1"),
-#     Uppdrag
-#   ))
+#### SVA DATA ####
 
-prrs.sva.retro <- read.retro.data("I:/ESS/SVA3D/PRRS/retro data/", 2009, 2018)
+html_pattern <- "<[^>]*>(.+)<[^>]*>"
 
-# import list of possible agens and their relevancy to PRRS
-agens.prrs.list <-
-  read.csv2(
-    file = "I:/ESS/SVA3D/PRRS/prrs.agens.csv",
+sva.retro <-
+  fread(
+    "I:/ESS/SVA3D/PRRS/retro data/sva.retro.2014-2018.txt",
+    header = TRUE,
+    na.strings = "",
+    stringsAsFactors = FALSE,
+    encoding = "UTF-8",
+    quote = "\"",
+    data.table = TRUE
+  )[str_detect(Uppdrag, html_pattern), Uppdrag := str_replace(Uppdrag, html_pattern, "\\1")] %>%
+  .[, Ankomstdatum := as.Date(Ankomstdatum, "%m/%d/%y")] %>%
+  .[is.na(Ankomstdatum), Ankomstdatum := as.Date(str_match(Uppdrag, "^U(\\d{6})")[, 2], format = "%y%m%d")] %>%
+  setnames(old = "Ankomstdatum", new = "date")
+
+# import list of possible agens and their relevance to PRRS
+sva.agens <-
+  fread(
+    "I:/ESS/SVA3D/PRRS/prrs.sva.agens.csv",
     encoding = "UTF-8",
     colClasses = "character",
     na.strings = ""
   )
 
 # extract only the agens which have been marked relevant to PRRS, and discard irrelevant columns
-agens.relevant <- agens.prrs.list %>%
-  filter(!(is.na(FINAL) | FINAL == "?" | FINAL == "x")) %>%
-  select(agens = X.U.FEFF.Agens, final = FINAL) %>%
-  mutate(final = as.numeric(final))
+sva.agens.relevant <- sva.agens %>%
+  .[!(is.na(FINAL) | FINAL == "?" | FINAL == "x"), .(Agens, final = FINAL)] %>%
+  .[, final := as.numeric(final)]
 
-undersokning.prrs.list <-
-  read.csv2(
-    file = "I:/ESS/SVA3D/PRRS/prrs.undersokning.csv",
+# repeat for undersökningar
+sva.undersokning <-
+  unique(fread(
+    "I:/ESS/SVA3D/PRRS/prrs.sva.undersokning.csv",
     encoding = "UTF-8",
     colClasses = "character",
     na.strings = ""
+  )) %>%
+  .[, mean(as.numeric(FINAL)), by = Kod]
+
+sva.undersokning.relevant <- sva.undersokning %>%
+  .[!is.na(V1), .(undersokning = Kod, final = V1)] %>%
+  .[, final := as.numeric(final)]
+
+# Find rows in dataset that have relevant agens and undersökning and attach value to them
+sva.with.agens <- sva.agens.relevant[sva.retro, on = "Agens"]
+
+sva.with.agens.undersokning <-
+  sva.undersokning.relevant[sva.with.agens,
+                            on = c("undersokning" = "Undersökning.Kod",
+                                   "undersokning" = "Analys.Kod")] %>%
+  setnames(
+    old = c("undersokning", "undersokning.1", "i.final", "final"),
+    new = c(
+      "Undersökning.Kod",
+      "Analys.Kod",
+      "PRRSagens",
+      "PRRSundersokning"
+    )
   )
 
-undersokning.relevant <- undersokning.prrs.list %>%
-  filter(!is.na(FINAL)) %>%
-  select(undersokning = X.U.FEFF.Kod, final = FINAL) %>%
-  mutate(final = as.numeric(final))
-
-# Find rows in dataset that have relevant agens and undersÃ¶kning and attach value to them
-prrs.sva.retro <- prrs.sva.retro %>%
-  left_join(agens.relevant, by = c("Agens" = "agens")) %>%
-  left_join(
-    undersokning.relevant,
-    by = c("UndersÃ¶kning.Kod" = "undersokning", "Analys.Kod" = "undersokning")
-  )
 
 # set all other rows' values to 0
-prrs.sva.retro <- prrs.sva.retro %>%
-  mutate(
-    PRRSagens = ifelse(is.na(final.x), 0, final.x),
-    PRRSundersokning = ifelse(is.na(final.y), 0, final.y)
-  ) %>%
-  select(-final.x, -final.y)
+sva.with.agens.undersokning[is.na(PRRSagens)]$PRRSagens <- 0
+sva.with.agens.undersokning[is.na(PRRSundersokning)]$PRRSundersokning <- 0
 
-prrs.sva.interesting <- prrs.sva.retro %>%
-  filter(PRRSagens > 0 | PRRSundersokning > 0)
+# load table with classification of provtagningsorsaker
+sva.provtagningsorsak <- fread("I:/ESS/SVA3D/PRRS/prrs.sva.provtagningsorsak.csv",
+                        encoding = "UTF-8",
+                        colClasses = "character",
+                        na.strings = "",
+                        stringsAsFactors = FALSE
+                        )
 
-prrs.passive.orsak <- prrs.sva.interesting %>%
-  filter(
-    Provtagningsorsak.1 == "Epizootisjukdom/ -misstanke" |
-      Provtagningsorsak.1 == "Sjukdom/Skada/DÃ¶dsfall"
+# add provtagningsorsak class to the data
+sva.agens.undersokning.origin <- merge(sva.with.agens.undersokning, sva.provtagningsorsak, all.x = T, by = "Provtagningsorsak.namn")
+sva.agens.undersokning.origin[is.na(ori)]$ori <- "Unclassified"
+
+# extract the rows with a PRRS-relevant agens or undersökning, and that are assumed to be passive surveillance cases 
+# (i.e. have a provtagningsorsak classified as passive surveillance or are missing an Överordnat uppdrag)
+sva.interesting <-
+  sva.agens.undersokning.origin[
+    (PRRSagens > 0 | PRRSundersokning > 0) & 
+      (ori == "Passive/unknown" | is.na(`Överordnade uppdrag`))]
+
+
+#### SJV DATA ####
+
+# read the data
+sjv.retro <- fread("I:/ESS/SVA3D/PRRS/retro data/sjv.retro.3800-days.txt",
+                   encoding = "UTF-8",
+                   na.strings = "",
+                   stringsAsFactors = FALSE
+                   )
+sjv.retro$Datum <- as.Date(sjv.retro$Datum)
+setnames(sjv.retro, old = "Datum", new = "date")
+
+# remove strange dates
+sjv.retro <- sjv.retro[date <= as.Date("2018-08-08")]
+
+# import diagnosis codes and their classifications
+sjv.diagnos <- fread(
+  "I:/ESS/SVA3D/PRRS/prrs.sjv.diagnos.csv",
+  encoding = "UTF-8",
+  na.strings = "",
+  stringsAsFactors = FALSE
+)
+
+# import handelsvara codes and their relevance to reproductive and/or respiratory diagnoses
+sjv.handelsvara <-
+  fread(
+    "I:/ESS/SVA3D/PRRS/prrs.sjv.handelsvara.csv",
+    encoding = "UTF-8",
+    na.strings = "",
+    stringsAsFactors = FALSE
   )
+sjv.handelsvara[is.na(Handelsvara.respiratory)]$Handelsvara.respiratory <- 0
+sjv.handelsvara[is.na(Handelsvara.reproductive)]$Handelsvara.reproductive <- 0
 
-prrs.no.orsak <- prrs.sva.interesting %>%
-  filter(is.na(Provtagningsorsak.1))
+### the following code looks messy, but essentially does the following:
+# 1. split the data into rows that have a handelsvarakod and rows that don't
+# 2. for the data that have handelsvarakod, make sure each code only appears once, 
+# but still keep all the variants of beskrivning by:
+#   2.1. grouping by handelsvarakod and saving a list of all beskrivning for each code
+#   2.2. for the respiratory and reproductive classifications, doing the same but only saving
+#        the first element of the lists (since all elements are identical for a specific 
+#        code)
+#   2.3. filtering out all (now) duplicate rows so that each row represents a unique handelsvarakod
 
-prrs.no.orsak.numberless <- prrs.no.orsak %>%
-  filter(is.na(Ã–verordnade.uppdrag))
+sjv.handelsvara.nocode <- sjv.handelsvara[is.na(Handelsvarakod)]
+sjv.handelsvara <- sjv.handelsvara[!is.na(Handelsvarakod)]
 
-prrs.sva.interesting <-
-  rbind(prrs.passive.orsak, prrs.no.orsak.numberless)
+sjv.handelsvara <-
+  unique(sjv.handelsvara[,
+                         c("names",
+                           "Handelsvara.respiratory",
+                           "Handelsvara.reproductive") :=
+                           list(list(c(Handelsvara.Beskrivning)),
+                                list(c(Handelsvara.respiratory))[[1]],
+                                list(c(Handelsvara.reproductive))[[1]]),
+                         by = Handelsvarakod] %>%
+           .[, .(
+             Handelsvara.Beskrivning = names,
+             Handelsvarakod,
+             Handelsvara.respiratory,
+             Handelsvara.reproductive
+           )],
+         by = "Handelsvarakod")
 
-interesting.events <- prrs.sva.interesting %>%
-  group_by(Uppdrag) %>%
-  summarise(date = Ankomstdatum[1])
+# add diagnos and handelsvara classification columns to original data
+# does not add list of possible beskrivning per handelsvarakod (for now)
+sjv.with.diagnos <- merge(sjv.retro,
+                          sjv.diagnos,
+                          by = "Diagnoskod",
+                          all.x = TRUE)
 
-date.range <-
-  as.data.frame(seq.Date(as.Date("2009-01-01"), as.Date("2018-12-31"), by = "day"))
-colnames(date.range) <- "date"
+sjv.with.diagnos.handelsvara <- merge(
+  sjv.with.diagnos,
+  sjv.handelsvara[, -"Handelsvara.Beskrivning"],
+  by = "Handelsvarakod",
+  all.x = TRUE
+)
+sjv.with.diagnos.handelsvara[is.na(Handelsvara.respiratory)]$Handelsvara.respiratory <- 0
+sjv.with.diagnos.handelsvara[is.na(Handelsvara.reproductive)]$Handelsvara.reproductive <- 0
 
-interesting.timeseries <- interesting.events %>%
-  group_by(date) %>%
-  count() %>%
-  right_join(date.range, by = "date") %>%
-  mutate(
-    n = ifelse(is.na(n), 0, n),
-    doy = as.numeric(strftime(date, format = "%j")),
-    m = month(date),
-    w = week(date),
-    y = as.factor(year(date))
-  )
+sjv.respiratory <-
+  sjv.with.diagnos.handelsvara[Diagnos.class == "Respiratory" |
+                                 Handelsvara.respiratory > 0]
+sjv.reproductive <-
+  sjv.with.diagnos.handelsvara[Diagnos.class == "Reproductive" |
+                                 Handelsvara.reproductive > 0]
 
-interesting.timeseries %>%
-  ggplot(aes(doy, n, color = y)) +
-  geom_line()
+sjv.any.code <- sjv.with.diagnos.handelsvara[!is.na(Diagnos.class)]
 
-interesting.timeseries %>%
-  group_by(w, y) %>%
-  summarise(n = sum(n)) %>%
-  ggplot(aes(x = w, y = n, color = y)) +
-  geom_line()
 
-interesting.timeseries %>%
-  group_by(y, w) %>%
-  summarise(n = sum(n)) %>%
-  group_by(w) %>%
-  summarise(n = mean(n)) %>%
-  ggplot(aes(w, n)) +
-  geom_line()
+#### TIMESERIES ####
+
+sva.all.dates <- data.table(date = seq.Date(min(sva.retro$date), max(sva.retro$date), by = "day"))
+sjv.all.dates <- data.table(date = seq.Date(min(sjv.retro$date), max(sjv.retro$date), by = "day"))
+
+sva.timeseries <- make_timeseries(sva.interesting, sva.all.dates)
+sjv.respiratory.series <- make_timeseries(sjv.respiratory, sjv.all.dates)
+sjv.reproductive.series <- make_timeseries(sjv.reproductive, sjv.all.dates)
+
+sva.timeseries[, sum(N), by = yearweek] %>%
+  ggplot(aes(yearweek, V1)) +
+  geom_line(group=1)
+
+sjv.respiratory.series[, sum(N), by = yearweek] %>%
+  ggplot(aes(yearweek, V1)) +
+  geom_line(group = 1)
+
+sjv.reproductive.series[, sum(N), by = yearweek] %>%
+  ggplot(aes(yearweek, V1)) +
+  geom_line(group = 1)
+
+
--- PRRS/prrs.sjv.classifications.r
+++ PRRS/prrs.sjv.classifications.r
@@ -1,32 +1,164 @@
+sjv.inespecific.death <- c("XB30", "XP15", "XP19", "XP25", "XP30")
 
-sjv.inespecific.death <- c("XB30","XP15","XP19","XP25","XP30")
-#XP15 = antibiotikabehandling, profylax
+sjv.inespecific.repro <-
+  c("XB66", "XB69", "XP40", "XP41", "XP42", "XP62", "XP63")
 
-sjv.inespecific.repro <- c("XB66","XB69","XP40","XP41","XP42","XP62","XP63")
+sjv.gris.allm <- c(
+  "002",
+  "010",
+  "011",
+  "013",
+  "017",
+  "020",
+  "090",
+  "091",
+  "AA009",
+  "AA01301",
+  "AA0129",
+  "AA01291",
+  "AA0134",
+  "AA031",
+  "AA002",
+  "AA0021"
+)
 
-sjv.gris.allm <- c("002","010","011","013","017","020","090","091",
-                      "AA009","AA01301","AA0129","AA01291","AA0134","AA031","AA002","AA0021")
+sjv.gris.circ <- c("100", "AA911")
 
-sjv.gris.circ <- c("100","AA911")
+sjv.gris.resp <- c(
+  "120",
+  "121",
+  "123",
+  "125",
+  "132",
+  "133",
+  "134",
+  "AA015",
+  "RA014",
+  "RA4199",
+  "RA4111",
+  "RB41",
+  "RB42",
+  "RB4192"
+)
 
-sjv.gris.resp <- c("120","121","123","125","132","133","134",
-                   "AA015","RA014","RA4199","RA4111","RB41","RB42","RB4192")
+sjv.gris.infek <-
+  c(
+    "300",
+    "360",
+    "361",
+    "365",
+    "366",
+    "367",
+    "368",
+    "370",
+    "371",
+    "377",
+    "430",
+    "431",
+    "438",
+    "439",
+    "454",
+    "AA0120",
+    "AA405",
+    "AA4051",
+    "AA40513",
+    "AA40514",
+    "AA40515",
+    "AA40517",
+    "AA40519",
+    "AA40520",
+    "AA40530",
+    "AA40668",
+    "AA40669",
+    "AA40677",
+    "AA4068",
+    "AA4314"
+  )
 
-sjv.gris.infek <- c("300","360","361","365","366","367","368","370","371","377",
-                        "430","431","438","439","454",
-                    "AA0120","AA405","AA4051","AA40513","AA40514","AA40515","AA40517","AA40519","AA40520","AA40530",
-                    "AA40668","AA40669","AA40677","AA4068","AA4314")
+sjv.gris.gift <- c("212", "570", "571",
+                   "MM2201", "AA111", "AA1111")
 
-sjv.gris.gift <- c("212","570","571",
-                   "MM2201","AA111","AA1111")
+sjv.gris.repro <- c(
+  "601",
+  "602",
+  "603",
+  "605",
+  "610",
+  "615",
+  "619",
+  "670",
+  "680",
+  "689",
+  "690",
+  "691",
+  "692",
+  "697",
+  "KA0142",
+  "KA0143",
+  "KA0144",
+  "KA0182",
+  "KA90",
+  "KA91",
+  "KA235",
+  "KA49",
+  "KB014",
+  "KB49",
+  "KC014",
+  "KC91",
+  "KC0112",
+  "KC401"
+)
 
-sjv.gris.repro <- c("601","602","603","605",
-                    "610","615","619","670","680","689","690","691","692","697",
-                    "KA0142","KA0143","KA0144","KA0182",
-                    "KA90","KA91","KA235","KA49","KB014","KB49","KC014","KC91","KC0112","KC401")
+sjv.gris.nerv <- c("838", "NJ91")
 
-sjv.gris.nerv <- c("838","NJ91")
+sjv.gris.oron <- c("865", "ÖB41")
 
-sjv.gris.oron <- c("865","Ã–B41")
+sjv.gris.hud <- c("905", "907", "HA41", "HA42")
 
-sjv.gris.hud <- c("905","907","HA41","HA42")
+all.codes = c(
+  sjv.inespecific.death,
+  sjv.inespecific.repro,
+  sjv.gris.allm,
+  sjv.gris.circ,
+  sjv.gris.resp,
+  sjv.gris.infek,
+  sjv.gris.gift,
+  sjv.gris.repro,
+  sjv.gris.nerv,
+  sjv.gris.oron,
+  sjv.gris.hud
+)
+
+combined = rbind(data.frame(
+  code = all.codes,
+  class = c(
+    rep("Unspecific death",
+        length(sjv.inespecific.death)),
+    rep("Unspecific reproductive",
+        length(sjv.inespecific.repro)),
+    rep("General",
+        length(sjv.gris.allm)),
+    rep("Circulatory",
+        length(sjv.gris.circ)),
+    rep("Respiratory",
+        length(sjv.gris.resp)),
+    rep("Infection",
+        length(sjv.gris.infek)),
+    rep("Poison",
+        length(sjv.gris.gift)),
+    rep("Reproductive",
+        length(sjv.gris.repro)),
+    rep("Nervous",
+        length(sjv.gris.nerv)),
+    rep("Ears",
+        length(sjv.gris.oron)),
+    rep("Skin",
+        length(sjv.gris.hud))
+  )
+))
+
+library(data.table)
+
+fwrite(combined,
+       file = "I:/ESS/SVA3D/PRRS/sjv.diagnosis.classes.csv",
+       sep = ";")
